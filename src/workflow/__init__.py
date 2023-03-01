0be189c299dbe2a99dc5a434a6b8f5993128e93b
Zhuwei Xu <zhuweix8@gmail.com>
2023-03-01 10:15:52 -0500

    minor update

--- src/workflow/__init__.py
+++ src/workflow/__init__.py
@@ -1,4 +1,5 @@
 __all__ = ['filter_split_zmw', 'generate_sbatch', 'workflow', 'indexreference', 'bamtobed', 'preprocess',
-           'filter_ccs', 'filter_subread', 'generate_sbatch_ipd', 'ipd_analysis', ]
+           'filter_ccs', 'filter_subread', 'generate_sbatch_ipd', 'ipd_analysis', 'workflow_local',
+           'generate_sbatch_ipd_local', 'generate_sbatch_local']
 __version__ = '1.0.0'
 __author__ = 'Zhuwei Xu'
